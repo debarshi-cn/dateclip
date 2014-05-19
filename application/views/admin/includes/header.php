b665cc6602f0c8416a82094ad672b4d75ff8cf43
debarshi-cn <debarshid@capitalnumbers.com>
2014-05-19 10:44:32 +0530

    further changes

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -12,11 +12,13 @@
 
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<p class="navbar-text">Welcome <a class="active"><?php echo $this->session->userdata('name');?></a>! <small>Last login: <?php echo $this->session->userdata('last_login');?></small>.</p>
+				<p class="navbar-text"><span class="glyphicon glyphicon-user"></span> Welcome <a class="active"><?php echo $this->session->userdata('name');?></a>! <small>Last login: <?php echo $this->session->userdata('last_login');?></small>.</p>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account<b class="caret"></b></a>
 					<ul class="dropdown-menu">
+						<li <?php echo $page == 'settings' ? 'class="active"' : '' ?>><a href="<?php echo HTTP_ADMIN_PATH; ?>settings">Manage Settings</a></li>
 						<li <?php echo $page == 'profile' ? 'class="active"' : '' ?>><a href="<?php echo HTTP_ADMIN_PATH; ?>home/profile">Profile</a></li>
+
 						<li class="divider"></li>
 						<li><a href="<?php echo HTTP_ADMIN_PATH; ?>home/logout">Logout</a></li>
 					</ul>
